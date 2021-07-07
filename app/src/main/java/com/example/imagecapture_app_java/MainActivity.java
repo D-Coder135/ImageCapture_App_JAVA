689e79ac69333580175182bb13618780d8a5111f
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-07 22:33:59 +0530

    add some fix up

--- app/src/main/java/com/example/imagecapture_app_java/MainActivity.java
+++ app/src/main/java/com/example/imagecapture_app_java/MainActivity.java
@@ -31,7 +31,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
     }
 }
