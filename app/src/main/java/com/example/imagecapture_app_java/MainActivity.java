97d780b01a68ac8765a8c81301d3150b5b75a917
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-07 22:32:05 +0530

    define the object for the class Intent

--- app/src/main/java/com/example/imagecapture_app_java/MainActivity.java
+++ app/src/main/java/com/example/imagecapture_app_java/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.imagecapture_app_java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -21,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             }
         });
     }
